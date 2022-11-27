dbe81d0b61519346b78b72412d43fb2d00c327c1
Olegin12 <olegrodionov12@gmail.com>
2022-11-28 01:00:38 +0300

    TODO: add gsap

--- js/app.js
+++ js/app.js
@@ -1,4 +1,15 @@
+import {gsap} from "../libs/gsap/gsap.min";
+import {ScrollTrigger} from "../libs/gsap/ScrollTrigger.min";
+import {ScrollSmoother} from "../libs/gsap/ScrollSmoother.min";
+
 window.addEventListener('scroll', e=> {
-    document.body.style.cssText = `--scrollTop: ${this.scrollY}px`
+    document.body.style.cssText += `--scrollTop: ${this.scrollY}px`
 })
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+ScrollSmoother.create({
+    wrapper: '.wrapper',
+    content: 'content'
+})
+
 
