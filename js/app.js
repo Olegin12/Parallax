b0ca878d6adf8b93eadb0cfd609db41d4cb700c4
Olegin12 <olegrodionov12@gmail.com>
2022-11-28 21:29:27 +0300

    TODO: add scroll smooth

--- js/app.js
+++ js/app.js
@@ -1,15 +1,3 @@
-import {gsap} from "../libs/gsap/gsap.min";
-import {ScrollTrigger} from "../libs/gsap/ScrollTrigger.min";
-import {ScrollSmoother} from "../libs/gsap/ScrollSmoother.min";
-
 window.addEventListener('scroll', e=> {
-    document.body.style.cssText += `--scrollTop: ${this.scrollY}px`
+    document.documentElement.style.setProperty('--scrollTop', `${this.scrollY}px`)
 })
-
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
-ScrollSmoother.create({
-    wrapper: '.wrapper',
-    content: 'content'
-})
-
-
